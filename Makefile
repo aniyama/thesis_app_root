dcfdbe072e338cacd48cd8f98faabffbab99044e
aniyama <anihide1017_2@yahoo.co.jp>
2022-09-18 18:12:37 +0900

    更新

---
+++ Makefile
@@ -0,0 +1,23 @@
+ne:
+	docker network ls
+
+in:
+	docker network inspect ${name}
+
+up:
+	docker-compose up
+
+dd:
+	docker-compose down
+
+dv:
+	docker-compose down --volumes
+
+up:
+	docker-compose ps -a
+
+db:
+	docker-compose exec db sh -c 'mysql -u $$MYSQL_USER -p$$MYSQL_PASSWORD $$MYSQL_DATABASE'
+
+dev:
+	cd ${name} && devcontainer open .
--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS category;
 
 CREATE TABLE users (
     id int not null auto_increment primary key,
-    name varchar(255) not null,
-    password varchar(255) not null
+    name varchar(32) not null,
+    password varchar(128) not null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE categories (
@@ -30,25 +30,25 @@ CREATE TABLE tags (
 CREATE TABLE modules (
     id int not null auto_increment primary key,
     title varchar(64) not null,
-    content varchar(255) not null,
+    content varchar(512) not null,
     user_id int not null,
     tag_id int ,
     created_at datetime,
     updated_at datetime,
     constraint fk_modules_user_id foreign key (user_id) references users (id) on delete cascade,
-    constraint fk_tag_id foreign key (tag_id) references tags (id) on delete cascade
+    constraint fk_tag_id foreign key (tag_id) references tags (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE theses (
     id int not null auto_increment primary key,
     thesis_title varchar(64) not null,
-    thesis_content varchar(255) not null,
+    thesis_content varchar(4096) not null,
     user_id int not null,
     tag_id int ,
     created_at datetime,
     updated_at datetime,
     constraint fk_thesis_user_id foreign key (user_id) references users (id) on delete cascade,
-    constraint fk_thesis_tag_id foreign key (tag_id) references tags (id) on delete cascade
+    constraint fk_thesis_tag_id foreign key (tag_id) references tags (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO categories (id, title) VALUES (1, 'THESIS');
